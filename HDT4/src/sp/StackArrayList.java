c6a98fcd36fc31d04193dc898d313bbf321179c0
Andre <andremt2003@gmail.co>
2023-02-15 18:26:37 -0600

    Vect y ARRAy

--- HDT4/src/sp/StackArrayList.java
+++ HDT4/src/sp/StackArrayList.java
@@ -4,4 +4,14 @@ import java.util.ArrayList;
 
 public class StackArrayList<T> extends AbstractStack<T>{
 
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
--- HDT4/src/sp/StackVector.java
+++ HDT4/src/sp/StackVector.java
@@ -4,4 +4,72 @@ import java.util.Vector;
 
 public class StackVector<T> extends AbstractStack<T> {
 
+    private T[] VECTOR;
+
+    public StackVector() {
+        VECTOR = (T[]) new Object[2];
+    }
+
+    private void alarga(){
+        T[] aux = (T[]) new Object[VECTOR.length*2];
+        System.arraycopy(VECTOR, 0, aux, 0, VECTOR.length);
+        VECTOR = aux;
+    }
+
+    private int IultimoDato(){
+        int i = 0;
+        for (int e = 0; e < VECTOR.length; e++) {
+            if(VECTOR[e] == null){
+                i = i-1;
+                return i;
+
+            }
+        }
+        return VECTOR.length-1;
+    }
+
+    public void push(T valor){
+        if(!isEmpty())
+            if(IultimoDato() == VECTOR.length-1) {
+                alarga();
+            }
+            else {
+                System.arraycopy(VECTOR, 0, VECTOR, 0, VECTOR.length - 1);
+            }
+        VECTOR[0] = valor;
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    public T peek(){
+        return VECTOR[0];
+    }
+
+    public int count(){
+        return IultimoDato()+1;
+    }
+
+    public boolean isEmpty(){
+        return VECTOR[0] == null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    public T pull(){
+        T aux = VECTOR[0];
+        if (IultimoDato()==VECTOR.length-1){
+            alarga();
+        }
+        else{
+            System.arraycopy(VECTOR, 1, VECTOR, 0, VECTOR.length-1);
+        }
+        return aux;
+    }
+
 }
