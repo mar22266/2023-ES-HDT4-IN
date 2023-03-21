5b3126c4f6ba89193d000db94b389276d61793f2
Andre <andremt2003@gmail.co>
2023-03-21 14:23:31 -0600

    algunas clases creadas

---
+++ HDT7/src/IBST.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+/**
+ *@Author: Andre Marroquin
+ * Estructuras de datos Hoja de trabajo 7
+ */
+public interface IBST<K,V> {
+
+    /**
+     * Metodo que inserta un valor en el arbol
+     * @param key
+     * @param value
+     */
+    void insert(K key, V value);
+
+    /**
+     * Metodo que busca un valor en el arbol
+     * @param key
+     * @return
+     */
+    V find(K key);
+
+    /**
+     * Metodo que elimina un valor en el arbol
+     * @param key
+     * @return
+     */
+    V delete(K key);
+
+    /**
+     * Metodo que retorna el numero de elementos en el arbol
+     * @return
+     */
+    int count();
+
+    /**
+     * Metodo que retorna si el arbol esta vacio
+     * @return
+     */
+
+    boolean isEmpty();
+
+    /**
+     * Metodo que retorna los elementos del arbol
+     * @return
+     */
+    ArrayList<V> getElements();
+
+}
---
+++ HDT7/src/ITraversal.java
@@ -0,0 +1,12 @@
+/**
+ *@Author: Andre Marroquin
+ * Estructuras de datos Hoja de trabajo 7
+ */
+public interface ITraversal<V> {
+
+    /**
+     * Metodo que recorre el arbol en preorden
+     * @param value
+     */
+    void Avanaza(V value);
+}
