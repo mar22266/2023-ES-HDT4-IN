2dd2e471631304e303af86fc0bb009ea0afdcd9c
Andre <andremt2003@gmail.co>
2023-04-19 12:20:10 -0600

    Listo, falta archivos txt por brindar y javadoc

---
+++ HDT9/src/Archivo.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author Andre marroquin, 22266
+ * HDT 9 - Estructura de datos
+ * clase para leer archivos
+ */
+
+public class Archivo {
+
+    /**
+     * Hace la lectura del archivo de texto y lo almacena en un mapa.
+     * @param tree
+     */
+    public void lectura(IMap tree){
+        try {
+            File file = new File("spanish.txt");
+            Scanner reader = new Scanner(file);
+            int i = 0;
+            while (reader.hasNextLine()) {
+                i++;
+                String dictionary = reader.nextLine();
+                String[]  words = dictionary.split("	");
+                String[] words2 = words[1].split(",");
+                tree.add(words[0], words2[0]);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado.");
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * hace la traduccion de las oraciones del archivo
+     * @param map
+     */
+    public void oracion(IMap map){
+        try {
+            File myObj = new File("texto.txt");
+            Scanner myReader = new Scanner(myObj);
+            ArrayList<String> lines = new ArrayList<>();
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+                lines.add(line);
+            }
+            myReader.close();
+
+            for (String data : lines) {
+                String[] temp = data.split("\\.");
+                StringBuilder res = new StringBuilder();
+                for (String s : temp) {
+                    String[] oracion = s.split(" ");
+                    System.out.println("\nOracion original: " + s + "\n");
+                    for(String i: oracion){
+                        if(map.find(i.toLowerCase())){
+                            res.append(" ");
+                            res.append(map.get(i));
+                            res.append(" ");
+                        } else{
+                            res.append(" ").append("*").append(i).append("*").append(" ");
+                        }
+                    }
+                }
+                System.out.println("\nTraduccion al espanol: " + res + "\n");
+            }
+        }catch (Exception e) {
+            System.out.println("Archivo no encontrado ERROR");
+            e.printStackTrace();
+        }
+    }
+}
--- HDT9/src/Main.java
+++ HDT9/src/Main.java
@@ -1,5 +1,67 @@
+import java.util.Scanner;
+
+/**
+ * @author Andre marroquin, 22266
+ * HDT 9 - Estructura de datos
+ */
+
+/**
+ * Clase main interaccion con el usuario
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IMap<String, String> datos;
+        String res = "";
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        System.out.println("\n-------Bienvenido al programa de traduccion de palabras------");
+        while (flag){
+            /**
+             * Menu para seleccionar el tipo de mapa
+             */
+            System.out.println("Ingrese el tipo de mapa que desea utilizar: ");
+            System.out.println("1. HashMap");
+            System.out.println("2. RBT");
+            System.out.println("3. Splay");
+            System.out.println("4. Salir");
+            Factory fact = new Factory();
+            int op = sc.nextInt();
+            switch (op){
+                //case 1: HashMapa
+                case 1:
+                    res = "HashMap";
+                    datos = fact.gett(res);
+                    Archivo rd = new Archivo();
+                    rd.lectura(datos);
+                    rd.oracion(datos);
+                    break;
+                case 2:
+                    //case 2: RBT
+                    res = "RBT";
+                    datos = fact.gett(res);
+                    Archivo rd2 = new Archivo();
+                    rd2.lectura(datos);
+                    rd2.oracion(datos);
+                    break;
+                case 3:
+                    //case 3: Splay
+                    res = "Splay";
+                    datos = fact.gett(res);
+                    Archivo rd3 = new Archivo();
+                    rd3.lectura(datos);
+                    rd3.oracion(datos);
+                    break;
+                case 4:
+                    //case 4: Salir
+                    flag = false;
+                    System.out.println("Gracias por utilizar el programa");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+
+        }
+
+        }
     }
-}
